f02fb20fc0ec18f0bcbaaddfa1862b2064ff426a
chiedo07 <chiedoluiz@gmail.com>
2025-07-23 13:03:17 +0100

    config files

--- project-2-cicd-pipeline/main.tf
+++ project-2-cicd-pipeline/main.tf
@@ -1,16 +1,28 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "acr_rg" {
-  name     = "Docker-container-registry"
-  location = "East UK"
+  name     = "Docker-acr-rg"
+  location = "East US"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "myuniqueacrname123" # must be globally unique
-  resource_group_name      = azurerm_resource_group.acr_rg.name
-  location                 = azurerm_resource_group.acr_rg.location
-  sku                      = "Standard"          # Choices: Basic, Standard, Premium
-  admin_enabled            = true                # Use false in production (use role-based access instead)
+  name                = "exampleacr12345"
+  resource_group_name = azurerm_resource_group.acr_rg.name
+  location            = azurerm_resource_group.acr_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
